4d0449c6b6bd4cbefcec4b10863c0cc37246e54f
EASYhz <65584699+EASYhz@users.noreply.github.com>
2020-09-23 21:06:08 +0900

    9/23

---
+++ ex1.java
@@ -0,0 +1,12 @@
+package example1;
+
+public class ex1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(1 > 1); // false
+		System.out.println(1 == 1); // true
+		
+	}
+
+}
---
+++ ex2.java
@@ -0,0 +1,23 @@
+package example1;
+
+public class ex2 {
+
+	public static void main(String[] args) {
+		
+		int a = 1;
+		
+		if(a < 3)
+		{
+			System.out.println("WOw");
+		}
+		else if (a == 4)
+		{
+			System.out.println("FAil");
+		}
+		else
+		{
+			System.out.println("Large");
+		}
+	}
+
+}
